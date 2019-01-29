1372d41547f3a2cf0ee5449e074a7e492ae53e17
Hiếu Ngọc <otimusblue@gmail.com>
2019-01-29 14:16:48 +0700

    Siblings,Find with content,.....

--- Scrapy-python/scrapetest.py
+++ Scrapy-python/scrapetest.py
@@ -3,14 +3,33 @@ from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import re
 
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
-#html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
-bsObj = BeautifulSoup(html)
+#SIMPLEST SCRAPING
+
+html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
+# print(html)
+# res = re.findall(r"<title>(.+?)</title>", html)
+# print("\nPage title is: ", res[0])
+
+
+#ALL PARAGRAPH
+# res = re.findall(r"<p>(.*?)</p>", html, flags=re.DOTALL)    # re.DOTALL if multi line
+# print("\nPage paragraph is: ", res[0])
+
+#ALL LINK
+res = re.findall(r'href="(.*?)"', html)
+print("\nAll links: ", res[0])
+
+
+#The .+? form requires at least one character to match
+
+#  html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+# html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+# bsObj = BeautifulSoup(html)
 #nameList = bsObj.findAll("span",{"class":"green"})
 # for name in nameList:
 #     print(name.get_text())
 
-# Find With Content
+#FIND WITH CONTENT
 
 # nameList = bsObj.findAll(text="the prince")
 # print(len(nameList))
@@ -34,9 +53,9 @@ bsObj = BeautifulSoup(html)
 
 #REGULAR EXPRESSIONS
 
-images = bsObj.findAll("img", {"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
-for image in images:
- print(image["src"])
+# images = bsObj.findAll("img", {"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+# for image in images:
+#  print(image["src"])
 
 
 
@@ -56,6 +75,9 @@ for image in images:
 #     print("Title could not be found")
 # else:
 #     print(title)
+
+#SIMPLEST SCRAPING
+
 
 
 
